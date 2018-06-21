a007f2b0fbba567c4f58ae3bf1d01c8269bd9818
PW486 <ooqwe486@gmail.com>
2018-06-21 17:21:48 +0900

    Add suggest user link

--- client/src/components/user_suggest/UserSuggestPage.vue
+++ client/src/components/user_suggest/UserSuggestPage.vue
@@ -4,7 +4,8 @@
     <h3><i class="fa fa-fw fa-link"></i> 관심사 기반 유저 매칭</h3>
     <div v-masonry transition-duration="1s" item-selector=".item" class="row">
       <div v-masonry-tile class="item col-3 my-2" v-for="user in users" v-bind:key="user.id">
-        <div class="card">
+        <a class="card" v-bind:href="'https://twitter.com/' + user.screen_name"
+          style="color:inherit;text-decoration:none;">
           <img class="card-img-top" v-bind:src="user.profile_image_url_https">
           <div class="card-body">
             <h5 class="card-title">{{ user.name }}
@@ -21,7 +22,7 @@
               {{ user.followers_count }} 팔로워
             </small>
           </div>
-        </div>
+        </a>
       </div>
     </div>
   </div>
