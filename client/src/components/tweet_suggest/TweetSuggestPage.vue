869553e191bcf7b6d7140a7b08f014ccf7e8a8bb
PW486 <ooqwe486@gmail.com>
2018-07-02 19:36:09 +0900

    Add keyword description

--- client/src/components/tweet_suggest/TweetSuggestPage.vue
+++ client/src/components/tweet_suggest/TweetSuggestPage.vue
@@ -5,7 +5,10 @@
       <h3><i class="fa fa-fw fa-thumbs-up"></i> 나를 위한 트윗 추천</h3>
       <div v-masonry transition-duration="1s" item-selector=".item" class="row">
         <div v-masonry-tile class="item col-4" v-for="tweet in tweets" v-bind:key="tweet.id">
-          <tweet-embed :id="tweet.id_str"></tweet-embed>
+          <h5><span class="badge badge-light btn-block keyword-badge">
+            {{ tweet.keyword }}
+          </span></h5>
+          <tweet-embed :id="tweet.id_str" style="margin-top:-8px;"></tweet-embed>
         </div>
       </div>
     </div>
--- client/src/components/user_suggest/UserSuggestPage.vue
+++ client/src/components/user_suggest/UserSuggestPage.vue
@@ -4,17 +4,21 @@
       <mini-profile></mini-profile><br>
       <h3><i class="fa fa-fw fa-link"></i> 관심사 기반 유저 매칭</h3>
       <div v-masonry transition-duration="1s" item-selector=".item" class="row">
-        <div v-masonry-tile class="item col-3 my-2" v-for="user in users" v-bind:key="user.id">
+        <div v-masonry-tile class="item col-4 my-2" v-for="user in users" v-bind:key="user.id">
           <a class="card" v-bind:href="'https://twitter.com/' + user.screen_name"
             style="color:inherit;text-decoration:none;">
             <img class="card-img-top" v-bind:src="user.profile_image_url_https">
             <div class="card-body">
+              <div v-if="user.location">
+                <small class="text-muted">{{ user.location }}</small><br>
+              </div>
               <h5 class="card-title">{{ user.name }}
                 <small class="text-muted">@{{ user.screen_name }}</small>
               </h5>
-              <p class="card-text">{{ user.description }}<br>
-                <small class="text-muted">{{ user.location }}</small>
-              </p>
+              <hr>
+              <div v-if="user.description">{{ user.description }}<br></div>
+              <small class="text-muted" v-for="keyword in user.keywords"
+                v-bind:key="keyword.id">{{keyword}} </small>
             </div>
             <div class="card-footer">
               <small class="text-muted">
